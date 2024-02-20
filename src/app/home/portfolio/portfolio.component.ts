f942aea79f8813c5491fc2166628ebfc8b1b8d2c
Nirakash Sapkota <83993087+Niracash@users.noreply.github.com>
2024-02-20 14:46:26 +0100

    projects changes

--- src/app/home/portfolio/portfolio.component.ts
+++ src/app/home/portfolio/portfolio.component.ts
@@ -1,21 +1,21 @@
 import { Component } from '@angular/core';
-import { gsap } from 'gsap/gsap-core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-portfolio',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './portfolio.component.html',
   styleUrl: './portfolio.component.css'
 })
 export class PortfolioComponent {
   projects: any[] = [
     {
-      name: 'Workout Buddy',
+      name: 'News Blog',
       type: 'Web Application',
-      description: 'A MERN project with JWT for logging exercises, tracking workout history, and monitoring progress.',
-      techs: ['dotnetcore', 'express', 'react', 'nodejs', 'tailwindcss'],
-      github: 'https://github.com/Oussemasahbeni/Workout-Tracker-mern-project',
+      description: 'The News Blog Web API is an easy-to-use website where people can read and post news articles. It has a special login area for managing everything, built with a simple design to keep things running smoothly.',
+      techs: ['dotnetcore', 'csharp', 'bootstrap', 'microsoftsqlserver'],
+      github: 'https://github.com/Niracash/NewsBlog',
       image: [
         'assets/workout buddy/login.jpg',
         'assets/workout buddy/signup.jpg',
@@ -25,11 +25,11 @@ export class PortfolioComponent {
 
     },
     {
-      name: 'Project Manger',
+      name: 'JobNotesAPI',
       type: 'Web Application',
-      description: 'Developed a Project Manager Website with Angular, PHP, and  MySQL, optimizing workflows, enhancing collaboration, and  ensuring seamless data management.',
-      techs: ['csharp', 'asp.net', 'angularjs', 'bootstrap'],
-      github: 'https://github.com/Oussemasahbeni/Angular-PHP-project-manager',
+      description: 'A simple note tracking app that tracks which company have been applied for job.',
+      techs: ['csharp', 'dotnetcore', 'angularjs', 'microsoftsqlserver'],
+      github: 'https://github.com/Niracash/JobNotesAPI',
       image: [
         'assets/projexus/login.jpg',
         'assets/projexus/signup.jpg',
@@ -40,11 +40,11 @@ export class PortfolioComponent {
 
     },
     {
-      name: 'Simon-Game      ',
+      name: 'Riot API',
       type: 'Web Application',
-      description: 'Created Simon Game in JavaScript, highlighting creative coding and game design for an engaging user experience',
-      techs: ['javascript', 'css3', 'html5',],
-      github: 'https://github.com/Oussemasahbeni/Simon-Game-Challenge',
+      description: 'A WPF project where you can track the stats in a game using Riot API. You can track player rank, games, levels etc',
+      techs: ['csharp', 'xml'],
+      github: 'https://github.com/Niracash/RiotAPI',
       image: [
         'assets/simongame/end.jpg',
         'assets/simongame/howto.jpg',
@@ -52,11 +52,11 @@ export class PortfolioComponent {
       ]
     },
     {
-      name: 'Products Managers ',
+      name: 'PC Health Checker',
       type: 'Web Application',
-      description: 'a Spring Boot /Angular application designed to provide a robust and secure authentication system with BCrypt for password hashing.The main goal of this project is to manage products and categories.',
-      techs: ['angularjs', 'spring', 'mysql', 'tailwindcss'],
-      github: 'https://github.com/Oussemasahbeni/restApiFront',
+      description: 'A python health script that constantly check health and make a log file with timestamp and error status when the pc gets problem',
+      techs: ['python'],
+      github: 'https://github.com/Niracash/PythonHealth',
       image: [
         'assets/product manager/categories.jpg',
         'assets/product manager/landing.jpg',
