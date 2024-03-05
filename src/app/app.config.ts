98197ffec9356d7e36bf13dd9d7645bb133fd43e
Nirakash Sapkota <83993087+Niracash@users.noreply.github.com>
2024-03-05 13:32:31 +0100

    Auto-generated commit

--- src/app/app.config.ts
+++
@@ -1,9 +0,0 @@
-import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
-import { provideHttpClient } from '@angular/common/http';
-import{provideHotToastConfig} from '@ngneat/hot-toast'
-import { routes } from './app.routes';
-
-export const appConfig: ApplicationConfig = {
-  providers: [provideHttpClient(), provideHotToastConfig(),]
-};
